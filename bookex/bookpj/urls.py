3ace8c4af4443b143ba9758e62bc3b9980b11dd8
sungbeom-hansol <psb6716@naver.com>
2017-01-17 15:18:34 +0900

    loginupdate

--- bookex/bookpj/urls.py
+++ bookex/bookpj/urls.py
@@ -3,10 +3,16 @@ from django.contrib import admin
 from bookpj.views import HomeView
 from django.conf.urls.static import static
 from django.conf import settings
+from bookpj.views import UserCreateView,UserCreateDoneTV
 
 urlpatterns = [
 	
 	url(r'^admin/', include(admin.site.urls)),
+
+	url(r'^accounts/', include('django.contrib.auth.urls')),
+	url(r'^accounts/register/$', UserCreateView.as_view(), name = 'register'),
+	url(r'^accounts/register/done/$', UserCreateDoneTV.as_view(), name = 'register_done'),
+
 	url(r'^$', HomeView.as_view(), name='home'),
 	url(r'^upload/', include('fileUp.urls', namespace='upfile')),
 
